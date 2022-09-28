d315fec149da5e981a2a9d3d9f41dadd66e8758f
eunbeann <ds5bean23@gmail.com>
2022-09-28 17:57:17 +0900

    feat: add useState

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import AppRouter from "./Router";
+
+function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+  return (
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    {/* JS 코드 쓸 땐 중괄호로 감싸주기 */}
+    <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+
+// 상위 컴포넌트에서 받은 프롭스 구조 분해 할당으로 사용
+const AppRouter = ({ isLoggedIn }) => {
+    return (
+        <Router>
+            {/* Switch 삭제 됨 > Routes로 변경 */}
+            <Routes>
+                {/* 삼항 연산자와 상태로 적절한 컴포넌트 반환 */}
+                {isLoggedIn ? (
+                    <Route exact path="/" element= { <Home /> }>
+                        {/* <Home />  Route 안에 component 대신 element 사용 v6 에러 수정*/ }
+                    </Route>
+                ) : (
+                    <Route exact path="/" element={ <Auth />}>
+                        {/* <Auth /> */}
+                    </Route>
+                )}
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter;
