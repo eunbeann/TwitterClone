9ed24e15771c279afd2a9966c85ab3942d9bd4ad
eunbeann <ds5bean23@gmail.com>
2022-09-30 17:27:45 +0900

    feat: Add SignIn"

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
-  // console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  console.log(authService.currentUser);
   return (
     <>
     <AppRouter isLoggedIn={isLoggedIn} />
     {/* JS 코드 쓸 땐 중괄호로 감싸주기 */}
-    <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
+      <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </>
   );
 }
