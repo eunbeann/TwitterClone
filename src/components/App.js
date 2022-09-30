1ca79a9162aa70a1c8e1093a77131e478be75add
eunbeann <ds5bean23@gmail.com>
2022-09-30 17:53:22 +0900

    feat:LogIn Delay

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  // const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
-  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  // 두번째 인자로 빈 []를 넣어주어야지 최초 렌더링 1회에만 동작
+  useEffect((() => {
+    authService.onAuthStateChanged((user) => console.log(user));
+  }, []))
+  // setInterval(() => console.log(authService.currentUser), 2000);
   return (
     <>
     <AppRouter isLoggedIn={isLoggedIn} />
