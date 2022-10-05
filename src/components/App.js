7c02602d804a42b2f5e67777e757f653e9c8dc57
eunbeann <ds5bean23@gmail.com>
2022-10-05 10:05:49 +0900

    feat: Logout"

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   // 두번째 인자로 빈 []를 넣어주어야지 최초 렌더링 1회에만 동작
-  useEffect((() => {
-    authService.onAuthStateChanged((user) => console.log(user));
-  }, []))
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      // null 값을 반환하는 불확실성 제거 
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   // setInterval(() => console.log(authService.currentUser), 2000);
   return (
     <>
-    <AppRouter isLoggedIn={isLoggedIn} />
-    {/* JS 코드 쓸 땐 중괄호로 감싸주기 */}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+      {/* JS는 중괄호로 감싸서 사용 */}
       <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </>
   );
