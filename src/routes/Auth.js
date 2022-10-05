2f9712894349086f93cd2867acc456ec103ca197
eunbeann <ds5bean23@gmail.com>
2022-10-05 17:47:47 +0900

    feat: Social LogIn

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import { async } from "@firebase/util";
 
 const Auth = () => {
@@ -8,6 +8,22 @@ const Auth = () => {
     const [newAccount, setnewAccount] = useState(true);
     const [error, setError] = useState("")
     const toggleAccount = () => setnewAccount((prev) => !prev);
+
+    const onSocialClick = async(event) => {
+        // console.log(event.target.name);
+        const {
+            target : {name},
+        } = event;
+        let provider;
+        if (name === "gooogle") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === "github") {
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data)
+        }
+    };
 
     const onChange = (event) => {
         const {
@@ -48,8 +64,8 @@ const Auth = () => {
                 {newAccount ? "Sign In" : "Create Account"}
             </span>
         <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
+            <button onClick={onSocialClick} namme="google">Continue with Google</button>
+            <button onClick={onSocialClick} namme="github">Continue with Github</button>
         </div>
     </div>
     )
